68bd370e773af481766e757b51667cdf2d817b48
Shefat Ahmed <shefata69@gmail.com>
2025-02-13 20:34:30 +0600

    conect RTK Query in redux and complete the proejct

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "./feature/todoSlice"
+import todoReducer from "./feature/todoSlice";
+import { baseApi } from "./api/api";
 export const store = configureStore({
-        reducer: {
-                todos : todoReducer
-        }
-})
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    todos: todoReducer,
+  },
+  middleware: (getDefaultMiddleware)=> getDefaultMiddleware().concat(baseApi.middleware)
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
